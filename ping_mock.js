d488d1ef611b34ad43a8a351f73b6c4fe48612d6
Mr-Rabbit146452001 <levanhungu652001@gmail.com>
2026-09-12 03:05:06 +0700

    feat: add root entrypoints for ping_mock and ping_unscanned

---
+++ ping_mock.js
@@ -0,0 +1,6 @@
+/**
+ * Root Proxy Runner cho ping_mock.js:
+ * Cho phép chạy "node ping_mock.js <lệnh>" ngay từ thư mục gốc của dự án StrollerApp
+ * mà không bắt buộc phải cd vào server/
+ */
+require('./server/ping_mock');
---
+++ ping_unscanned.js
@@ -0,0 +1,5 @@
+/**
+ * Root Proxy Runner cho ping_unscanned.js:
+ * Cho phép chạy "node ping_unscanned.js" ngay từ thư mục gốc của dự án StrollerApp
+ */
+require('./server/ping_unscanned');
